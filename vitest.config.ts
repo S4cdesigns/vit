bff15039cb04186253c9676b2715e3b519eb3be9
boi12321 <pornvault@protonmail.com>
2022-11-18 19:37:34 +0100

    coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "istanbul",
+    },
     globalSetup: ["test/setup.ts"],
     exclude: ["testx", "node_modules", "build", ".next", ".test"],
     env: {
