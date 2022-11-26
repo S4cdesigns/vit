0c44650c57df7f50b03e7fddbb19a5b90da6d1e9
boi12321 <pornvault@protonmail.com>
2022-11-26 15:15:30 +0100

    cres

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -10,7 +10,21 @@ export default function Credits() {
           </a>
           )
         </li>
+        <li>globochem</li>
+        <li>Herpes3000</li>
+        <li>DGs.Ch00</li>
+        <li>GernBlanston</li>
+        <li>john4valor</li>
       </ul>
+      <div>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://gitlab.com/porn-vault/porn-vault/-/blob/ssr/components/Credits.tsx"
+        >
+          Edit on GitLab
+        </a>
+      </div>
     </>
   );
 }
